ef476119b9c3b714c653fba814dc6aa952ab7587
EngineerMaruf <softengmaruf@gmail.com>
2024-01-02 18:51:45 +0600

    conversion js completed

---
+++ conversion.js
@@ -0,0 +1,17 @@
+var price1 = 31;
+var price2 = 10;
+
+var totalPrice = price1 + price2;
+// console.log(totalPrice);
+
+var firstName = 'Maruf';
+var lastName = 'Siddique';
+var fullName = firstName + ' ' + lastName;
+// console.log(fullName);
+
+
+
+// convert string to number 
+var price3 = '25';
+price3 = parseInt(price3);
+console.log(price3);
--- operation.js
+++ operation.js
@@ -9,3 +9,12 @@ console.log(totalPrice);
 
 var priceDifference = onionPrice - eggPrice;
 console.log(priceDifference);
+
+
+//add something to the previous value
+var parice1 = 31;
+var price2 = 10;
+
+// add value with a value 
+price1 = price1 + 10;
+console.log(price1);
