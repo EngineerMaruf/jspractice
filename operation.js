9bd50bcaf352f47ccf5e3ca37523f743ebb3ae5f
EngineerMaruf <softengmaruf@gmail.com>
2024-01-02 16:58:47 +0600

    practice js

---
+++ operation.js
@@ -0,0 +1,11 @@
+var onionPrice =20;
+var eggPrice = 10;
+
+console.log(onionPrice);
+console.log(eggPrice);
+
+var totalPrice = onionPrice + eggPrice ;
+console.log(totalPrice);
+
+var priceDifference = onionPrice - eggPrice;
+console.log(priceDifference);
