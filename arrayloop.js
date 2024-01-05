d61a73e2ec425a8048b9968d3cdb3d3052f0780d
EngineerMaruf <softengmaruf@gmail.com>
2024-01-05 10:21:40 +0600

    arrayloop

---
+++ arrayloop.js
@@ -0,0 +1,10 @@
+for(i=0; i<10; i++){
+    console.log(i);
+}
+
+var numbers = [45, 55, 65, 258, 235];
+// display all element of this Array
+for(var i=0; i<7; i++){
+    var number = numbers[i];
+console.log(number);
+}
---
+++ for.js
@@ -0,0 +1,8 @@
+for( var roastGiven = 0; roastGiven < 7; roastGiven++){
+    console.log('roast den, please');
+}
+
+// simple version of for loop 
+for(i=0; i<7; i++){
+    console.log(i);
+}
